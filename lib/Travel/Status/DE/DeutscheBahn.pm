10e549ef7479b1bd08ced124164b5edcd8c1a30b
Daniel Friesel <derf@finalrewind.org>
2011-07-06 19:46:54 +0200

    Release v0.02

--- lib/Travel/Status/DE/DeutscheBahn.pm
+++ lib/Travel/Status/DE/DeutscheBahn.pm
@@ -10,7 +10,7 @@ use POSIX qw(strftime);
 use Travel::Status::DE::DeutscheBahn::Result;
 use XML::LibXML;
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 sub new {
 	my ( $obj, %conf ) = @_;
@@ -203,7 +203,7 @@ arrival/departure monitor
 
 =head1 VERSION
 
-version 0.01
+version 0.02
 
 =head1 DESCRIPTION
 
--- lib/Travel/Status/DE/DeutscheBahn/Result.pm
+++ lib/Travel/Status/DE/DeutscheBahn/Result.pm
@@ -6,7 +6,7 @@ use 5.010;
 
 use parent 'Class::Accessor';
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 Travel::Status::DE::DeutscheBahn::Result->mk_ro_accessors(
 	qw(time train route_end route_raw platform info));
@@ -71,7 +71,7 @@ arrival/departure received by Travel::Status::DE::DeutscheBahn
 
 =head1 VERSION
 
-version 0.01
+version 0.02
 
 =head1 DESCRIPTION
 
